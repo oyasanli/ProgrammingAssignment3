c2dfa2c18e51ca7924337dd6ad9f6adb4342f0f5
oyasanli <46657728+oyasanli@users.noreply.github.com>
2020-11-19 21:42:29 +0300

    Update rankall.R

--- rankall.R
+++ rankall.R
@@ -1,52 +1,67 @@
 ##Function finds the hospital in a state in giving outcome and rank. 
 ##Oya Şanlı
-rankAll <- function(outcome, num = "best") {
-
-    dataAll <- data.frame(hospital = character(), state = character())
-  
-## Read data
-
-    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  
-## Check that outcome and num are valid
-
-    if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
-        dataAll <- "invalid outcome"
-    }
-    else {
-        keys <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
-        outcomeKey <- keys[outcome]
-
-## For each state, find the hospital of the given rank
-##split data by state
-        data_state <- split(data, data$State)
-##for every state find the best hospitals
-        for (stat in names(data_state)) {
-           our_state <- data_state[[stat]]
-           data_outcome <- suppressWarnings(as.numeric(our_state[, outcomeKey]))
-           good <- complete.cases(data_outcome)
-           data_outcome <- data_outcome[good]
-           our_state <- our_state[good,]
-##order by outcome and hospital name
-           our_state <- our_state[ order(data_outcome, our_state["Hospital.Name"]), ]
-        
-           if (num == "best") {
-               numState <- c(1)
-           } else {
-               if (num == "worst") {
-                   numState <- length(data_outcome)
-               } else {
-                   numState <- num
-               }
-           }
+rankall <- function(outcome, num = 'best') {
+## Read outcome data
     
-        dataPart <- data.frame(hospital = our_state[numState, "Hospital.Name"], state = stat, row.names = stat)
-##combine all data
-        dataAll <- rbind(dataAll, dataPart)
+    outcomes <- read.csv("outcome-of-care-measures.csv", 
+                         colClasses = "character",
+                         header = TRUE)
+    
+## Get data 
+    
+    rates <- as.data.frame(cbind(outcomes[, 2],   # hospital
+                                 outcomes[, 7],   # state
+                                 outcomes[, 11],  # heart attack
+                                 outcomes[, 17],  # heart failure
+                                 outcomes[, 23]), # pneumonia
+                           stringsAsFactors = FALSE)
+    
+## Rename columns
+    
+    colnames(rates) <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
+    
+## Check outcome 
+   
+    if(!outcome %in% c("heart attack", "heart failure", "pneumonia")){
+        stop('invalid outcome')
+    }
+    
+## Return hospital name in that state with lowest 30-day death
+   
+    hRank <- data.frame()
+##€ery state    
+    for(state in sort(unique(rates[,"state"]))){
+        
+## Get the hospitals
+        hRates <- rates[(rates[, "state"] == state), ]
+        
+## Convert outcome rate to numberic, gets a warning
+        hRates[, outcome] <- as.numeric(hRates[, outcome])
+        
+## Remove NA values
+        hRates <- hRates[!is.na(hRates[, outcome]), ]
+        
+## convert num argument to valid rank
+        
+        if(num == "best") {
+            rnum <- 1 
+        } else if (num == "worst") {
+            rnum <- nrow(hRates) 
         }
+        else {rnum = num}
+        
+        
+## Order by outcome rate & hospital name
+        hRates <- hRates[order(hRates[, outcome], hRates[, "hospital"]), ]
+        
+        hName <- hRates[rnum,1]
+        
+        hRank <- rbind(hRank,
+                       data.frame(hospital = hName,
+                                  state = state))
     }
 
-## Return the data frame with the hospital names and state
-
-    dataAll
+## Return dataframe
+    hRank
+    
 }
